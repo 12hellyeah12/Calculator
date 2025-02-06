b01f4b95e0f6f5922a42fc4d75adf50e335845d8
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-02-06 21:53:22 +0100

    Add logic to get user input and do the calculation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const inputFrame = document.querySelector("#input-frame");
 const outputFrame = document.querySelector("#output-frame");
+const inputHistory = document.querySelector("#input-history");
 
 const buttonText = [
     "", "+/-", "%", "/",
      "7", "8", "9", "*", 
      "4", "5", "6", "-",
      "1", "2", "3", "+",
-     "AC", "0", ",", "="
+     "AC", "0", ".", "="
 ];
 
 const calculatorFunctions = {
@@ -15,7 +16,7 @@ const calculatorFunctions = {
     "*": (num1, num2) => num1 * num2,
     "/": (num1, num2) => num1 / num2,
     "%": (num1, num2) => num1 % num2,
-}
+};
 
 const createInputButtons = (numOfButtons) => {
     for(let i = 0; i < numOfButtons; i++){
@@ -30,16 +31,49 @@ const nameInputButtons = () => {
     for(let i = 0; i < 20; i++){
         buttons[i].innerText = buttonText[i];
         buttons[i].id = buttonText[i];
+        buttons[i].value = buttonText[i];
     }
-    
 };
 
 const operate = (num1, operator, num2) => {
-    const calc = calculatorFunctions[operator](num1, num2);
+    const calc = calculatorFunctions[operator](Number(num1), Number(num2));
     outputFrame.innerText = calc;
+};
+
+const resetCalculator = () => {
+    outputFrame.innerText = "";
+    inputHistory.innerText = "";
+};
+
+const executeCalculation = () => {
+    const operationsArray = outputFrame.innerText.split("");
+    const indexOfOperator = operationsArray
+    .map(string => Number(string))
+    .findIndex(num => num != num);
+
+    const numberOne = operationsArray.splice(0, indexOfOperator).join("");
+    const operator = operationsArray.splice(0, 1);
+    const numberTwo = operationsArray.splice(0).join("");
+
+    operate(numberOne, operator, numberTwo);
 }
 
 createInputButtons(20);
 nameInputButtons();
-operate(3, "+", 5);
+//operate(3, "+", 5);
+
+// adds function to buttons 0-9
+buttons.forEach((button) => {
+    if(button.id === "AC") {
+        button.addEventListener("click", () => {
+            resetCalculator();
+        });
+    } else if(button.id === "="){
+        button.addEventListener("click", () => {
+            executeCalculation();
+        });
+    } else {
+        button.addEventListener("click", () => outputFrame.innerText += button.value);
+    } 
+});
 
