b04bd612a4da6565aed081dfe1617de446e17e57
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-02-10 16:00:22 +0100

    Add functionality to +/- button

--- script.js
+++ script.js
@@ -59,9 +59,11 @@ const resetCalculator = () => {
 
 const executeCalculation = () => {
     const operationsArray = outputFrame.innerText.split("");
-    const indexOfOperator = operationsArray
-    .map(string => Number(string))
-    .findIndex(num => num != num);
+    const indexOfOperator = operationsArray.findIndex((element, index) => {
+        if(index > 0){
+        return operators.includes(element);
+        }
+    });
 
     const numberOne = operationsArray.splice(0, indexOfOperator).join("");
     const operator = operationsArray.splice(0, 1);
@@ -71,18 +73,21 @@ const executeCalculation = () => {
 }
 
 const checkForCorrectInput = (button, input) => {
-    const doubleOperatorRegex = /^\d+[%+-/*]\d+$/; 
-    const replaceLastOperatorRegex = /^\d+[%+-/*]$/; 
+    const doubleOperatorRegex = /^-?\d+[%+-/*]-?\d+$/; 
+    const replaceLastOperatorRegex = /^-?\d+[%+-/*]$/; 
+    const positiveToNegativeRegex = /^-?\d+$/;
 
     if(doubleOperatorRegex.test(input.innerText)){
         executeCalculation();
+       input.innerText += button.value;
     } else if(replaceLastOperatorRegex.test(input.innerText)){
         const stringToArray = input.innerText.split("");
         stringToArray.pop();
         input.innerText = stringToArray.join("");
+        input.innerText += button.value;
+    } else if(positiveToNegativeRegex.test(input.innerText) && button.value === "+/-"){
+        operate(input.innerText, "*", "-1");
     }
-
-    input.innerText += button.value;
 };
 
 const toggleOperatorButtons = () => {
@@ -99,6 +104,7 @@ const toggleOperatorButtons = () => {
 
 createInputButtons(20);
 nameInputButtons();
+toggleOperatorButtons();
 
 buttons.forEach(button => {
     if(numbers.includes(button.value)){
@@ -108,7 +114,10 @@ buttons.forEach(button => {
             toggleOperatorButtons();
         });
     } else if(operators.includes(button.value)){
-        button.addEventListener("click", () => checkForCorrectInput(button, outputFrame));
+        button.addEventListener("click", () => {
+            checkForCorrectInput(button, outputFrame);
+            outputFrame.innerText += button.value;
+        });
     } else if(button.value === "="){
         button.addEventListener("click", () => executeCalculation());
     } else if(button.value === "AC"){
@@ -116,6 +125,10 @@ buttons.forEach(button => {
             resetCalculator();
             clickCount = 0;
             toggleOperatorButtons();
+        });
+    } else if(button.value === "+/-"){
+        button.addEventListener("click", () => {
+            checkForCorrectInput(button, outputFrame);
         });
     }
 })
